1dc374190d1b752f7d805ef09e42393a3771d435
IzumiSy <beetle-noise@gmx.com>
2019-03-16 13:36:04 +0900

    整理

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  margin: 0;
+  padding: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Wrapper className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.tsx</code> and save to reload.
+            f4employee-app
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
-      </div>
+      </Wrapper>
     );
   }
 }
