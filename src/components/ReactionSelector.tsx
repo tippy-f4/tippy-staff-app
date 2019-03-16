3f90dc18a209a6acac872aa912ee11db3121d04c
IzumiSy <beetle-noise@gmx.com>
2019-03-16 16:22:34 +0900

    u

--- src/components/ReactionSelector.tsx
+++ src/components/ReactionSelector.tsx
@@ -13,7 +13,7 @@ const Selector = styled.div`
   box-shadow: 0 1px 5px 0 rgba(195, 195, 195, 0.5);
   border-radius: 10px;
   transition: all .3s cubic-bezier(0.4, 0.0, 0.2, 1);
-  transform: translateY(${(props: SelectorProps) => props.isOpen ? "-25px" : "0"});
+  transform: translateY(${(props: SelectorProps) => props.isOpen ? "-30px" : "0"});
   visibility: ${(props: SelectorProps) => props.isOpen ? 'visible' : 'hidden' };
   opacity: ${(props: SelectorProps) => props.isOpen ? '1' : '0'};
   background: white;
