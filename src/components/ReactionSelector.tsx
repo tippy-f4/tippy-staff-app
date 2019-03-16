19830e206902825be2c00b8647449cf4b3d9ece2
IzumiSy <beetle-noise@gmx.com>
2019-03-16 16:17:26 +0900

    u

--- src/components/ReactionSelector.tsx
+++ src/components/ReactionSelector.tsx
@@ -1,30 +1,43 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
+const Toggler = styled.div`
   text-align: right;
 `
 
-const Opening = styled.div``
+interface SelectorProps {
+  isOpen: Boolean
+}
 
-const Closed = styled.div``
+const Selector = styled.div`
+  box-shadow: 0 1px 5px 0 rgba(195, 195, 195, 0.5);
+  border-radius: 10px;
+  transition: all .3s cubic-bezier(0.4, 0.0, 0.2, 1);
+  transform: translateY(${(props: SelectorProps) => props.isOpen ? "-25px" : "0"});
+  visibility: ${(props: SelectorProps) => props.isOpen ? 'visible' : 'hidden' };
+  opacity: ${(props: SelectorProps) => props.isOpen ? '1' : '0'};
+  background: white;
+  position: absolute;
+  z-index: 200;
+  right: 0;
+`
+
+const Wrapper = styled.div`
+  width: 100%;
+  position: relative;
+`
 
 export const ReactionSelector = () => {
   const [isOpen, setOpenStatus] = useState(false)
 
   return (
-    <Wrapper onClick={() => setOpenStatus(!isOpen)}>
-      {
-        isOpen ? (
-          <Opening>
-            opening
-          </Opening>
-        ) : (
-          <Closed>
-            closed
-          </Closed>
-        )
-      }
+    <Wrapper>
+      <Selector isOpen={isOpen}>
+        hoge
+      </Selector>
+      <Toggler onClick={() => setOpenStatus(!isOpen)}>
+        click
+      </Toggler>
     </Wrapper>
   )
 }
