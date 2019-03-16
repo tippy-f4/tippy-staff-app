95b022957430f5d0846158ad4514e4167eedab67
IzumiSy <beetle-noise@gmx.com>
2019-03-16 15:01:25 +0900

    u

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,41 +1,41 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { CardVM } from '../cardVM';
 
-const Wrapper = styled.div`
-  padding: 10px;
-  margin: 16px 10px;
-  border-radius: 9px;
-  box-shadow: 0 1px 5px 0 rgba(195, 195, 195, 0.5);
-  background-color: #ffffff;
-`
-
-const Message = styled.div`
-  line-height: 1.5;
-  font-size: 14px;
-  letter-spacing: normal;
-  color: #9b9b9b;
-  font-family: HiraginoSans-W3;
-  padding: 14px 0;
-`
-
-const FacePicture = styled.img`
-  border-radius: 50%;
-  display: inline-block;
-  vertical-align: middle;
-  height: 40px
-  width: 40px;
-`
-
-const Name = styled.div`
-  display: inline-block;
-  font-size: 12px;
-  color: #9b9b9b;
-  margin-left: 11px;
-  font-weight: bold;
-`
-
 export const Card = (card: CardVM) => {
+  const Wrapper = styled.div`
+    padding: 10px;
+    margin: 16px 10px;
+    border-radius: 9px;
+    box-shadow: 0 1px 5px 0 rgba(195, 195, 195, 0.5);
+    background-color: #ffffff;
+  `
+
+  const Message = styled.div`
+    line-height: 1.5;
+    font-size: 14px;
+    letter-spacing: normal;
+    color: #9b9b9b;
+    font-family: HiraginoSans-W3;
+    padding: 14px 0;
+  `
+
+  const FacePicture = styled.img`
+    border-radius: 50%;
+    display: inline-block;
+    vertical-align: middle;
+    height: 40px
+    width: 40px;
+  `
+
+  const Name = styled.div`
+    display: inline-block;
+    font-size: 12px;
+    color: #9b9b9b;
+    margin-left: 11px;
+    font-weight: bold;
+  `
+
   return (
     <Wrapper>
       <FacePicture src={card.employeePictureUrl} />
@@ -45,6 +45,39 @@ export const Card = (card: CardVM) => {
       <Message>
         {card.message}
       </Message>
+      <ReactionSelector />
+    </Wrapper>
+  )
+}
+
+const ReactionSelector = () => {
+  const [isOpen, setOpenStatus] = useState(false)
+
+  const Wrapper = styled.div`
+    text-align: right;
+  `
+
+  const Opening = styled.div`
+
+  `
+
+  const Closed = styled.div`
+
+  `
+
+  return (
+    <Wrapper onClick={() => setOpenStatus(!isOpen)}>
+      {
+        isOpen ? (
+          <Opening>
+            opening
+          </Opening>
+        ) : (
+          <Closed>
+            closed
+          </Closed>
+        )
+      }
     </Wrapper>
   )
 }
